0f38c716d021e19192839c9c91e9b7cfccf35dd7
Pascal vetsch <pvetsch@hsr.ch>
2012-11-07 21:17:27 +0100

    test ui and package edit

--- src/ui/view/MainFrame.java
+++ src/ui/view/MainFrame.java
@@ -6,10 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JButton btnHide_1;
+	private JButton btnHide;
 
 	/**
 	 * Launch the application.
@@ -35,8 +43,49 @@ public class MainFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 0, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		contentPane.add(panel, gbc_panel);
+		
+		btnHide = new JButton("hide");
+		btnHide.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnHide.setVisible(false);
+				btnHide_1.setVisible(true);
+			}
+		});
+		panel.add(btnHide);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 1;
+		gbc_panel_1.gridy = 0;
+		contentPane.add(panel_1, gbc_panel_1);
+		
+		btnHide_1 = new JButton("hide");
+		btnHide_1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				btnHide.setVisible(true);
+				btnHide_1.setVisible(false);
+			}
+		});
+		panel_1.add(btnHide_1);
+		btnHide_1.setVisible(false);
 	}
 
 }
