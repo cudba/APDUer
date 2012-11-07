c272e8da4707d89b8b75a0fa42842f00a9985a24
Curdin Barandun <cbarandun@gmail.com>
2012-11-07 18:46:18 +0100

    test3

---
+++ src/model/test.java
@@ -0,0 +1,9 @@
+package model;
+
+public class test {
+	
+	public static void main(String[] args) {
+		System.out.println("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
+	}
+
+}
