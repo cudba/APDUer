b557236a1c4e12a0118e1b998faf40430e7b4db7
Pascal vetsch <pvetsch@hsr.ch>
2012-11-07 20:46:35 +0100

    test

--- src/model/test.java
+++ src/model/test.java
@@ -5,7 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
 		System.out.println("42, probably..");
-		System.out.println("asdasd");
 	}
 
 }
