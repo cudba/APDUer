2970f85c08dfb532c8badfd98d33e26ac5768d82
Curdin Barandun <cbarandun@gmail.com>
2012-11-07 17:29:14 +0100

    testcommit

--- src/ui/test.java
+++ src/ui/test.java
@@ -1,5 +1,9 @@
 package ui;
 
 public class test {
+	
+	public static void main(String[] args) {
+		System.out.println("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
+	}
 
 }
