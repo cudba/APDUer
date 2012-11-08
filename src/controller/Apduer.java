83df36268aa27d6accc73efdebaac0d1e3827074
Curdin Barandun <cbarandun@gmail.com>
2012-11-08 16:40:26 +0100

    todo added

--- src/controller/Apduer.java
+++ src/controller/Apduer.java
@@ -4,10 +4,13 @@ import ui.view.MainFrame;
 
 public class Apduer {
 
+	//TODO: move into another package
 	
 	public static void main(String[] args) {
 		
 		MainFrame view = new MainFrame();
+		
+		//cause output on console
 		view.setVisible(false);
 
 		RelayController controller = new RelayController();
