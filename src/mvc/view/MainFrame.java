70268b03657e063e5662a7a8c1fcdfdfe50833b5
Pascal Vetsch <pascalvetsch@hotmail.com>
2012-11-11 22:36:41 +0100

    view update

--- src/mvc/view/MainFrame.java
+++ src/mvc/view/MainFrame.java
@@ -14,14 +14,17 @@ import javax.swing.border.EmptyBorder;
 import mvc.listener.ApduListener;
 import mvc.model.Apdu;
 import mvc.model.ApduData;
+import javax.swing.JTable;
 
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JButton btnHide_1;
-	private JButton btnHide;
 	
 	private ApduData data;
+	private JTable table;
+	private JPanel panel_options;
+	private JButton btnTrap;
+	private JButton btnSend;
 
 	/**
 	 * Launch the application.
@@ -53,47 +56,52 @@ public class MainFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 81, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
-		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.insets = new Insets(0, 0, 0, 5);
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 0;
-		contentPane.add(panel, gbc_panel);
+		JPanel panel_table = new JPanel();
+		GridBagConstraints gbc_panel_table = new GridBagConstraints();
+		gbc_panel_table.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_table.fill = GridBagConstraints.BOTH;
+		gbc_panel_table.gridx = 0;
+		gbc_panel_table.gridy = 0;
+		contentPane.add(panel_table, gbc_panel_table);
 		
-		btnHide = new JButton("hide");
-		btnHide.addActionListener(new ActionListener() {
+		table = new JTable();
+		panel_table.add(table);
+		
+		panel_options = new JPanel();
+		GridBagConstraints gbc_panel_options = new GridBagConstraints();
+		gbc_panel_options.fill = GridBagConstraints.BOTH;
+		gbc_panel_options.gridx = 0;
+		gbc_panel_options.gridy = 1;
+		contentPane.add(panel_options, gbc_panel_options);
+		GridBagLayout gbl_panel_options = new GridBagLayout();
+		gbl_panel_options.columnWidths = new int[]{0, 0, 0};
+		gbl_panel_options.rowHeights = new int[]{0, 0};
+		gbl_panel_options.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_options.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_options.setLayout(gbl_panel_options);
+		
+		btnTrap = new JButton("Trap");
+		GridBagConstraints gbc_btnTrap = new GridBagConstraints();
+		gbc_btnTrap.insets = new Insets(0, 0, 0, 5);
+		gbc_btnTrap.gridx = 0;
+		gbc_btnTrap.gridy = 0;
+		panel_options.add(btnTrap, gbc_btnTrap);
+		
+		btnSend = new JButton("Send trapped APDU");
+		btnSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				btnHide.setVisible(false);
-				btnHide_1.setVisible(true);
 			}
 		});
-		panel.add(btnHide);
-		
-		JPanel panel_1 = new JPanel();
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 1;
-		gbc_panel_1.gridy = 0;
-		contentPane.add(panel_1, gbc_panel_1);
-		
-		btnHide_1 = new JButton("hide");
-		btnHide_1.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				btnHide.setVisible(true);
-				btnHide_1.setVisible(false);
-			}
-		});
-		panel_1.add(btnHide_1);
-		btnHide_1.setVisible(false);
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.gridx = 1;
+		gbc_btnSend.gridy = 0;
+		panel_options.add(btnSend, gbc_btnSend);
 	}
 	
 	
