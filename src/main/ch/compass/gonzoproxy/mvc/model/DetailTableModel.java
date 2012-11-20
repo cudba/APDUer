31e8605403a397dab0a276543b3ea53f7d84f499
Pascal Vetsch <pascalvetsch@hotmail.com>
2012-11-20 15:12:10 +0100

    table model, command apdu

---
+++ src/main/ch/compass/gonzoproxy/mvc/model/DetailTableModel.java
@@ -0,0 +1,53 @@
+package ch.compass.gonzoproxy.mvc.model;
+
+import javax.swing.table.AbstractTableModel;
+
+public class DetailTableModel extends AbstractTableModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1437358812481945385L;
+	private Apdu apdu;
+	private String[] columnNames = { "Field", "Value", "Description" };
+	  
+	public DetailTableModel(Apdu apdu) {
+		this.apdu = apdu;
+	}
+
+	public String getColumnName(int col) {
+		return this.columnNames[col].toString();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return this.columnNames.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return apdu.getFields().size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Field field = apdu.getFields().get(rowIndex);
+
+		switch (columnIndex) {
+		case 0:
+			return field.getName();
+		case 1:
+			return field.getValue();
+		case 2:
+			return field.getDescription();
+		}
+		return null;
+
+	}
+
+	public void setApdu(Apdu editApdu) {
+		this.apdu = editApdu;
+		fireTableDataChanged();
+	}
+
+}
